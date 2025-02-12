275f68378fb005171fa31082d4116b40ced75242
SamSmith2004 <23388986@studentmail.ul.ie>
2025-02-12 16:24:02 +0000

    setup basic TCP server

--- Server/src/main/java/ul/Server/App.java
+++ Server/src/main/java/ul/Server/App.java
@@ -1,7 +1,50 @@
 package ul.Server;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+// nc localhost 8080
+
 public class App {
+    private static final int PORT = 8080;
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        try (ServerSocket servSock = new ServerSocket(PORT)) {
+            System.out.println("Server listening on port " + PORT);
+
+            while (true) {
+                Socket link = null;
+                try {
+                    link = servSock.accept();
+                    // System.out.println("New connection from " + link.getRemoteAddress());
+
+                    BufferedReader in =
+                            new BufferedReader(new InputStreamReader(link.getInputStream()));
+                    PrintWriter out = new PrintWriter(link.getOutputStream(), true);
+
+                    String message;
+                    while ((message = in.readLine()) != null) {
+                        // System.out.println("Received: " + message);
+                        out.println("Server: " + message);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        if (link != null && !link.isClosed()) {
+                            link.close();
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
