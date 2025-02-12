6979cdc2f190c0f077f19adc4e3609da132fe60d
SamSmith2004 <23388986@studentmail.ul.ie>
2025-02-12 14:39:33 +0000

    Updated project structure to 1 maven package with 2 modules

--- Client/src/main/java/ul/cs4076project/App.java
+++ Client/src/main/java/ul/cs4076project/App.java
@@ -11,7 +11,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("View/main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1600, 900);
+        Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
         stage.setTitle("Lecture Timetable");
         stage.setScene(scene);
         stage.show();
