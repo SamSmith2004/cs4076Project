2a7823974b1bb9d6e0968aac51f6e53760db0e01
SamSmith2004 <23388986@studentmail.ul.ie>
2025-02-18 15:33:38 +0000

    Exception is more explicit than throwable

--- Server/src/main/java/ul/Server/Utils/IncorrectActionException.java
+++ Server/src/main/java/ul/Server/Utils/IncorrectActionException.java
@@ -1,6 +1,6 @@
 package ul.Server.Utils;
 
-public class IncorrectActionException extends Throwable{
+public class IncorrectActionException extends Exception {
     public IncorrectActionException() {
         super("Incorrect action event");
     }
