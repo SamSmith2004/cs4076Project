f6f9348d468f91a7e260fbba33b995462f695011
SamSmith2004 <23388986@studentmail.ul.ie>
2025-02-18 10:24:33 +0000

    remove skeleton

--- Client/src/main/java/ul/cs4076project/Controller/RemoveALecturePopupDialogueController.java
+++ Client/src/main/java/ul/cs4076project/Controller/RemoveALecturePopupDialogueController.java
@@ -3,15 +3,21 @@ package ul.cs4076project.Controller;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
+import ul.cs4076project.Model.TCPClient;
 
 public class RemoveALecturePopupDialogueController {
     private Stage removeALecturePopupStage;
+    private TCPClient client;
 
     @FXML
     private Button okButton;
 
     public void setDialogStage(Stage dialogStage) {
         this.removeALecturePopupStage = dialogStage;
+    }
+
+    public void initializeWithClient(TCPClient client) {
+        this.client = client;
     }
 
     @FXML
