ead0c04a5a22a75fd7f7c2df3fe0c9bcd727e9d8
ilias <i.k.kourousis@gmail.com>
2025-02-18 16:53:03 +0000

    styling changes to timetable view

--- Client/src/main/java/ul/cs4076project/Controller/TimetableController.java
+++ Client/src/main/java/ul/cs4076project/Controller/TimetableController.java
@@ -10,7 +10,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import ul.cs4076project.App;
 import ul.cs4076project.Model.Lecture;
@@ -21,10 +21,13 @@ public class TimetableController implements Initializable {
     private String[][] timetable;
     private Lecture[][] lectures;
 
-    @FXML private Label debugLabel;
-    @FXML private GridPane timetableGrid;
+    @FXML
+    private Label debugLabel;
+    @FXML
+    private GridPane timetableGrid;
 
-    public TimetableController() {}
+    public TimetableController() {
+    }
 
     public void initializeWithClient(TCPClient client) {
         this.client = client;
@@ -41,7 +44,7 @@ public class TimetableController implements Initializable {
         try {
             for (int row = 1; row < 10; row++) {
                 for (int col = 0; col < 5; col++) {
-                    Pane cellPane = new Pane();
+                    StackPane cellPane = new StackPane();
                     cellPane.setStyle("-fx-background-color: white; -fx-border-color: lightgray;");
                     cellPane.setMinSize(100, 50);
                     cellPane.setPrefSize(100, 50);
@@ -65,10 +68,10 @@ public class TimetableController implements Initializable {
             Map<String, String> headers = new HashMap<>();
             headers.put("Content-Type", "timetable");
 
-            Object response = (Lecture[][])client.get("LM05125", headers);
+            Object response = (Lecture[][]) client.get("LM05125", headers);
             if (response != null) {
                 timetable = new String[5][9];
-                lectures = (Lecture[][])response;
+                lectures = (Lecture[][]) response;
                 updateTimetableGrid(lectures);
             } else {
                 debugLabel.setText("Error: Invalid response type");
@@ -81,28 +84,30 @@ public class TimetableController implements Initializable {
 
     private void updateTimetableGrid(Lecture[][] lectures) {
         createEmptyCells();
-        
+
         for (int row = 1; row < 10; row++) {
             for (int col = 0; col < 5; col++) {
                 Lecture lecture = lectures[col][row - 1];
                 if (lecture != null) {
                     timetable[col][row - 1] = lecture.getModule();
 
-                    Pane cellPane = new Pane();
+                    StackPane cellPane = new StackPane();
                     cellPane.setStyle("-fx-background-color: white; -fx-border-color: lightgray;");
                     cellPane.setMinSize(100, 50);
                     cellPane.setPrefSize(100, 50);
 
                     VBox labelContainer = new VBox(2);
                     labelContainer.setStyle("-fx-padding: 5;");
+                    labelContainer.setAlignment(javafx.geometry.Pos.CENTER); // Center the labels within the VBox
                     labelContainer.getChildren().addAll(
-                            createStyledLabel(lecture.getModule(), "-fx-font-weight: bold;"),
-                            createStyledLabel(lecture.getLecturer(), "-fx-font-size: 10;"),
-                            createStyledLabel(lecture.getRoom(), "-fx-font-size: 10;"),
-                            createStyledLabel(lecture.getTime(), "-fx-font-size: 10;")
-                    );
+                            createStyledLabel(lecture.getModule(), "-fx-font-size: 16; -fx-font-weight: bold;"),
+                            createStyledLabel(lecture.getLecturer(), "-fx-font-size: 16;"),
+                            createStyledLabel(lecture.getRoom(), "-fx-font-size: 16;"),
+                            createStyledLabel(lecture.getTime(), "-fx-font-size: 16;"));
 
                     cellPane.getChildren().add(labelContainer);
+                    StackPane.setAlignment(labelContainer, javafx.geometry.Pos.CENTER); // Center the VBox within the
+                                                                                        // StackPane
                     timetableGrid.add(cellPane, col, row);
                 }
             }
@@ -113,6 +118,7 @@ public class TimetableController implements Initializable {
     private Label createStyledLabel(String text, String style) {
         Label label = new Label(text);
         label.setStyle(style);
+        label.setAlignment(javafx.geometry.Pos.CENTER); // Center the text within the label
         return label;
     }
 
