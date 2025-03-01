68a5b3eef116f0565216b19061af72c9aa9bda9a
SamSmith2004 <23388986@studentmail.ul.ie>
2025-03-01 14:45:41 +0000

    binary testing

--- Client/src/main/java/module-info.java
+++ Client/src/main/java/module-info.java
@@ -1,10 +1,10 @@
 module ul.cs4076project {
     requires javafx.controls;
     requires javafx.fxml;
-
     requires org.controlsfx.controls;
     requires org.kordamp.ikonli.javafx;
     requires jakarta.json;
+    requires org.eclipse.parsson;
 
     opens ul.cs4076project to javafx.fxml;
     exports ul.cs4076project;
