1d740a7c07a29a29133e5b83d2e0ca02a7119364
Ilias Kourousis <i.k.kourousis@gmail.com>
2025-03-07 14:37:08 +0000

    Javadoc comments for ResponseType

--- Client/src/main/java/ul/cs4076project/Model/ResponseType.java
+++ Client/src/main/java/ul/cs4076project/Model/ResponseType.java
@@ -1,7 +1,26 @@
 package ul.cs4076project.Model;
 
-// ResponseType is a sealed interface that defines the types of responses that can be returned from the server
+/**
+ * ResponseType is a sealed interface that defines the types of responses
+ * that can be returned from the server.
+ * 
+ * <p>
+ * This interface is sealed, meaning only the specified classes can implement
+ * it. It has two implementing records: {@link StringResponse} and
+ * {@link TimetableResponse}.
+ */
+
 public sealed interface ResponseType {
-    record StringResponse(String value) implements ResponseType {}
-    record TimetableResponse(Lecture[][] value) implements ResponseType {}
+    /**
+     * Represents a response that contains a single string value.
+     */
+    record StringResponse(String value) implements ResponseType {
+    }
+
+    /**
+     * Represents a response that contains a 2D array of {@link Lecture} objects,
+     * representing a timetable.
+     */
+    record TimetableResponse(Lecture[][] value) implements ResponseType {
+    }
 }
